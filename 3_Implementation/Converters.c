41a5d2f9f42787afa1200845bcd58df2bfc1bf7f
Kowsik15 <kowsikshanmugham15@gmail.com>
2022-02-18 13:36:42 +0530

    Added files via upload

--- 3_Implementation/Converters.c
+++ 3_Implementation/Converters.c
@@ -1,5 +1,7 @@
-#include <stdio.h>
+#include "Converters.h"
+
 int main()
+
 {
     char choose_type;
     int temp_Choice;
@@ -10,35 +12,13 @@ int main()
     float usrinputC, usroutputC;              // Usr gives values for Celsius;
     float usroutputKC, usrinputK, usroutputK; // Usr gives values for Kelvin
 
-    float fahrenheitToCelcius(float t1) { return ((9.0 / 5.0) * t1 + 32); }
-    // function that stores the converted F->C;
-
-    float celciusToFahrenheit(float t2) { return ((t2 - 32) * (5.0 / 9.0)); }
-    // function that stores the converted C->F;
-    float kelvinToCelcius(float t3) { return (t3 - 273.15); }
-    // function that stores the converted K->C;
-    float celciusToKelvin(float t4) { return (t4 + 273.15); }
-    // function that stores the converted C->K;
-
     float usrinputUSDtoEuro, usrinputUSDtoJPY, usrinputUSDtoRMB;
     float usroutputUSDtoEuro, usroutputUSDtoJPY, usroutputUSDtoRMB;
-
-    float USDtoEuro(float c1) { return (c1 * 0.87); }
-    // function to store USD to EURO;
-    float USDtoJPY(float c2) { return (c2 * 111.09); }
-    // function to store USD to JPY;
-    float USDtoRMB(float c3) { return (c3 * 6.82); }
-    // function to store USD to RMB;
 
     float usrinputOunce, usroutputPound;
     // Usr gives values for for Ounce;
     float usrinputGram;
     // Usr gives values for for Gram;
-
-    float ounceToPounds(float m1) { return (m1 * 111.09); }
-    // stores the converted Ounce->Pounds;
-    float gramsToPounds(float m2) { return (m2 * 0.00220462); }
-    // stores the converted Grams->Pounds;
 
     printf("Welcome to Unit Converter! \nHere is a list of conversation to choose from: \nTemperature(T),\nCurrency(C),\nMass(M) \nPlease enter the codeword you want to convert.\n");
     scanf("%c", &choose_type);
---
+++ 3_Implementation/Converters.h
@@ -0,0 +1,27 @@
+#ifndef CONVERTERS_H_
+#define CONVERTERS_H_
+
+#include <stdio.h>
+float fahrenheitToCelcius(float t1) { return ((9.0 / 5.0) * t1 + 32); }
+// function that stores the converted F->C;
+
+float celciusToFahrenheit(float t2) { return ((t2 - 32) * (5.0 / 9.0)); }
+// function that stores the converted C->F;
+float kelvinToCelcius(float t3) { return (t3 - 273.15); }
+// function that stores the converted K->C;
+float celciusToKelvin(float t4) { return (t4 + 273.15); }
+// function that stores the converted C->K;
+
+float USDtoEuro(float c1) { return (c1 * 0.87); }
+// function to store USD to EURO;
+float USDtoJPY(float c2) { return (c2 * 111.09); }
+// function to store USD to JPY;
+float USDtoRMB(float c3) { return (c3 * 6.82); }
+// function to store USD to RMB;
+
+float ounceToPounds(float m1) { return (m1 * 111.09); }
+// stores the converted Ounce->Pounds;
+float gramsToPounds(float m2) { return (m2 * 0.00220462); }
+// stores the converted Grams->Pounds;
+
+#endif
